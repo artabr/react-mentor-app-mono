fdec5c82b7fb7dbef7854b2f1b30fbb5ae87e1d8
Artem Abramov <Artem_Abramov1@epam.com>
2022-12-01 01:53:05 +0300

    serverless-http

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,7 +1,4 @@
+const serverless = require('serverless-http');
 const app = require('./app')
 
-const port = process.env.PORT || 4000
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+module.exports.handler = serverless(app);
